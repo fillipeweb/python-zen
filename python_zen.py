0acc478f56f9096d20cb1ee173274b6231c48e3e
Fillipe Cordeiro <fillipe.cordeiro@concretesolutions.com.br>
2013-09-21 13:26:19 -0300

    Special Hello World! using Flask Frawework

---
+++ python_zen.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+zen = """
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+@app.route("/")
+def index():
+	return render_template('content.html', zen=zen)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
